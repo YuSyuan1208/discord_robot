2ba339dbbee279183e030d9324aba8f4c80b19ca
yusyuan1208 <yusyuan1208@gmail.com>
2020-07-20 22:19:22 +0800

    [IMP]keep alive open

--- main.py
+++ main.py
@@ -242,5 +242,5 @@ for filename in os.listdir('./cmds'):
         bot.load_extension(f'cmds.{filename[:-3]}')
 
 if __name__ == "__main__":
-    # keep_alive.keep_alive()
+    keep_alive.keep_alive()
     bot.run(setting_data['TOKEN'])
